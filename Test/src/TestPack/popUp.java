97587ab13db89e6d5a1a6d385946dd8d939c967a
SAROKOK <SAROKOK@L-156071262.wipro.com>
2018-05-24 23:01:25 -0500

    First Commit

---
+++ Test/src/TestPack/popUp.java
@@ -0,0 +1,10 @@
+package TestPack;
+
+import javax.swing.JOptionPane;
+
+public class popUp {
+	public static void infoBox(String infoMessage, String titleBar)
+    {
+        JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
+    }
+}
